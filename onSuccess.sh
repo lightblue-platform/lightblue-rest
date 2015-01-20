e645e1539a0c302a6ecff98e35c132ff60e94ce0
Naveen Malik <nmalik@redhat.com>
2015-01-20 11:14:23 -0500

    Moved cobertura to onSuccess.sh, removed travis install section (did not move)

--- onSuccess.sh
+++ onSuccess.sh
@@ -3,3 +3,4 @@
 echo "DEPLOY MASTER TRAVIS BUILD"
 echo "Current directory is $(pwd)"
 '[[ $TRAVIS_BRANCH == "master" ]] && [[ $TRAVIS_JDK_VERSION == "openjdk7" ]] && { mvn clean deploy -DskipTests; };'
+mvn -B clean verify cobertura:cobertura coveralls:cobertura -Dorg.apache.maven.user-settings=~/settings-arquillian.xml  -Dorg.apache.maven.global-settings=~/settings-arquillian.xml
