96206e80ce14bb978c3e427ac1d00e673d78a9ec
Naveen Malik <nmalik@redhat.com>
2016-01-22 10:33:59 -0500

    Operation checkers now check if context is null

--- common/src/main/java/com/redhat/lightblue/rest/audit/LightblueMetadataOperationChecker.java
+++ common/src/main/java/com/redhat/lightblue/rest/audit/LightblueMetadataOperationChecker.java
@@ -70,9 +70,14 @@ public enum LightblueMetadataOperationChecker implements LightblueOperationCheck
         return operation;
     }
 
+    @Override
     public Info matches(String content){
-        Matcher matcher = getPattern().matcher(content);
-        boolean found = matcher.matches();
+        Matcher matcher = null;
+        boolean found = false;
+        if (content == null) {
+            matcher = getPattern().matcher(content);
+            found = matcher.matches();
+        }
         return new Info(getOperation(),found,matcher, moreInfo);
     }
 }
